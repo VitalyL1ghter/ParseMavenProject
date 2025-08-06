a4164a96270296d16b8a9aacfe46a83cbec9d913
VitalyL1ghter <stalker-kzn@mail.ru>
2025-08-06 12:56:54 +0300

    Проект дз 13 parse was added new

--- src/main/java/com/example/parse/utils/ByCondition.java
+++ src/main/java/com/example/parse/utils/ByCondition.java
@@ -1,7 +1,5 @@
 package com.example.parse.utils;
 
-import java.util.List;
-
 @FunctionalInterface
 public interface ByCondition <T extends Number> {
     boolean isOK (T number);
--- src/main/java/com/example/parse/utils/Sequence.java
+++ src/main/java/com/example/parse/utils/Sequence.java
@@ -1,10 +1,8 @@
 package com.example.parse.utils;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Sequence {
     public static List<Number> filterNegativeNumber(Number[] array, ByCondition condition) {
